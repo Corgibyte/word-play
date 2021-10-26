b3aa84b00b4c54b50bb30ff2155e16d947688f68
Hannah Y <hannah@corgibyte.com>
2021-10-25 22:22:08 -0700

    Add functionality to sort word counts by number of repeats.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
-function output(outputWord, outputCount) {
-  $("#output").append("<li>" + outputWord + ": " + outputCount + "</li>");
+function output(wordCounts) {
+  wordCounts.forEach(function(wordCount) {
+    $("#output").append("<li>" + wordCount + "</li>");
+  });
+}
+
+function insertWordCount(oldArray, newWord, newCount) {
+  let newArray = [];  
+  const newElement = newWord + ": " + newCount;
+  if (oldArray.length < 1) {
+    newArray.push(newElement);
+  } else {
+    let inserted = false;
+    oldArray.forEach(function(element) {
+      elementCount = parseInt(element.split(": ")[1]);
+      if (newCount > elementCount && !inserted) {
+        newArray.push(newElement);
+        inserted = true;
+      }
+      newArray.push(element);
+    });
+    if (!inserted) {
+      newArray.push(newElement);
+    }
+  }
+  return newArray;
 }
 
 $(document).ready(function() {
@@ -13,16 +37,18 @@ $(document).ready(function() {
 
     let lastCount = 0;
     let lastWord = "";
+    let wordCounts = [];
     origSentenceArr.forEach(function(word) {
       if (word === lastWord) {
         lastCount += 1;
       } else {
         if (lastWord !== "") {
-          output(lastWord, lastCount);
+          wordCounts = insertWordCount(wordCounts, lastWord, lastCount);
         }
         lastWord = word;
         lastCount = 1;        
       }              
     });
+    output(wordCounts);
   });
 });
